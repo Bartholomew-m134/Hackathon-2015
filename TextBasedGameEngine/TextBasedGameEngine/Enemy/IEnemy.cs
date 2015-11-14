d32a33e7153de1d8329f350f656c47b6ba82e1e3
John Cramer <cramer.240@buckeyemail.osu.edu>
2015-11-14 18:05:31 -0500

    Missing IEnemy

---
+++ TextBasedGameEngine/TextBasedGameEngine/Enemy/IEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedGameEngine.Enemy
+{
+    public interface IEnemy
+    {
+        int AttackPower
+        {
+            get;
+        }
+
+        int Health
+        {
+            get;
+            set;
+        }
+
+        bool Hostile
+        {
+            get;
+        }
+
+        bool HitWithMagic
+        {
+            get;
+            set;
+        }
+
+        void HandleMagicBurn();
+    }
+}
