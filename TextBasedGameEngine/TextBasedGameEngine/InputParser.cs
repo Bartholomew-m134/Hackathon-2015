c800b017753ec92fe53a195707287cde11abbb8c
Matt Bartholomew <Bartholomew.m134@gmail.com>
2015-11-14 14:09:04 -0500

    Starting InputParser

---
+++ TextBasedGameEngine/TextBasedGameEngine/InputParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedGameEngine
+{
+    public static class InputParser
+    {
+        public static void PlayerCommandGetCommand() 
+        { 
+            
+        }
+    }
+}
