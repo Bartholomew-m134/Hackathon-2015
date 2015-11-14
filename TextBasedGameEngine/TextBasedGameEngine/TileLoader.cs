f144e901516719ac861c96a66ac68568f77798db
John Cramer <cramer.240@buckeyemail.osu.edu>
2015-11-14 15:45:37 -0500

    Added some to the tile loader.

--- TextBasedGameEngine/TextBasedGameEngine/TileLoader.cs
+++ TextBasedGameEngine/TextBasedGameEngine/TileLoader.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextBasedGameEngine.Player;
+using TextBasedGameEngine.Tiles;
 
 namespace TextBasedGameEngine
 {
     public static class TileLoader
     {
+        public static ITile MoveNorth(IPlayer player, ITile tile)
+        {
+            int nextYPosition = player.Position.Y - 1;
+            ITile nextTile = tile;
+            return nextTile;
+        }
 
+        public static ITile MoveSouth(IPlayer player, ITile tile)
+        {
+            int nextYPosition = player.Position.Y + 1;
+            ITile nextTile = tile;
+            return nextTile;
+        }
+
+        public static ITile MoveWest(IPlayer player, ITile tile)
+        {
+            int nextXPosition = player.Position.X - 1;
+            ITile nextTile = tile;
+            return nextTile;
+        }
+
+        public static ITile MoveEast(IPlayer player, ITile tile)
+        {
+            int nextXPosition = player.Position.X + 1;
+            ITile nextTile = tile;
+            return nextTile;
+        }
     }
 }
